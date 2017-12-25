2cd43c49d3783ce9fbfa54417e53ecfd8a90cd16
Nevan Tan <nevan@tanclan.ca>
2017-12-25 12:11:47 -0500

    Setup webpack for development

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = env => {
+  return {
+    entry: ['./src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devtool: 'cheap-eval-course-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
+  };
+};
