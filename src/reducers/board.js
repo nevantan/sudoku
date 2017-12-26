54f9af56568c6c0d0739ee20b28caf46582c1da2
Nevan Tan <nevan@tanclan.ca>
2017-12-25 19:36:50 -0500

    Integrate Redux store to project

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,31 +1,43 @@
 import validateRow from '../validators/row.js';
 import validateColumn from '../validators/column.js';
 import validateCage from '../validators/cage.js';
+import { emptyBoard } from '../tests/fixtures/boards.js';
 
-export default (state, action) => {
-  const newBoard = state.board.map((row, y) => {
-    if (y == action.row) {
-      return row.map((cell, x) => {
-        if (x == action.col) return { value: action.value, valid: true };
-        return cell;
+const defaultState = { board: emptyBoard };
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case 'UPDATE_CELL':
+      const newBoard = state.board.map((row, y) => {
+        if (y == action.row) {
+          return row.map((cell, x) => {
+            if (x == action.col) return { value: action.value, valid: true };
+            return cell;
+          });
+        }
+        return row;
       });
-    }
-    return row;
-  });
 
-  const rowValid = validateRow(newBoard, action.row);
+      const rowValid = validateRow(newBoard, action.row);
 
-  const board = newBoard.map((row, y) => {
-    return row.map((cell, x) => {
-      const colValid = validateColumn(newBoard, x);
-      const cageValid = validateCage(newBoard, coordToCage(y, x));
-      return { value: cell.value, valid: rowValid && colValid && cageValid };
-    });
-  });
+      const board = newBoard.map((row, y) => {
+        return row.map((cell, x) => {
+          const colValid = validateColumn(newBoard, x);
+          const cageValid = validateCage(newBoard, coordToCage(y, x));
+          return {
+            value: cell.value,
+            valid: rowValid && colValid && cageValid
+          };
+        });
+      });
 
-  return {
-    board
-  };
+      return {
+        ...state,
+        board
+      };
+    default:
+      return state;
+  }
 };
 
 const coordToCage = (row, col) => {
