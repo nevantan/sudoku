8cdb532f89128878400f49d15cca6f04ed99d320
Nevan Tan <nevan@tanclan.ca>
2017-12-27 18:48:11 -0500

    Add basic styles for board layout and cell error

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Cage from './Cage';
 
 const Board = () => (
-    <div>
-        <p>Board Component</p>
-        <Cage cage={0} />
-        <Cage cage={1} />
-        <Cage cage={2} />
-        <Cage cage={3} />
-        <Cage cage={4} />
-        <Cage cage={5} />
-        <Cage cage={6} />
-        <Cage cage={7} />
-        <Cage cage={8} />
+    <div className="board">
+        <div className="board__row">
+            <Cage cage={0} />
+            <Cage cage={1} />
+            <Cage cage={2} />
+        </div>
+
+        <div className="board__row">
+            <Cage cage={3} />
+            <Cage cage={4} />
+            <Cage cage={5} />
+        </div>
+
+        <div className="board__row">
+            <Cage cage={6} />
+            <Cage cage={7} />
+            <Cage cage={8} />
+        </div>
     </div>
 );
 
--- src/components/Cage.js
+++ src/components/Cage.js
@@ -12,49 +12,58 @@ class Cage extends React.Component {
   };
   render() {
     return (
-      <div>
-        <p>Cage Component</p>
-        <Cell id={'' + this.row + this.col} row={this.row} column={this.col} />
-        <Cell
-          id={'' + this.row + (this.col + 1)}
-          row={this.row}
-          column={this.col + 1}
-        />
-        <Cell
-          id={'' + this.row + (this.col + 2)}
-          row={this.row}
-          column={this.col + 2}
-        />
-        <Cell
-          id={'' + (this.row + 1) + this.col}
-          row={this.row + 1}
-          column={this.col}
-        />
-        <Cell
-          id={'' + (this.row + 1) + (this.col + 1)}
-          row={this.row + 1}
-          column={this.col + 1}
-        />
-        <Cell
-          id={'' + (this.row + 1) + (this.col + 2)}
-          row={this.row + 1}
-          column={this.col + 2}
-        />
-        <Cell
-          id={'' + (this.row + 2) + this.col}
-          row={this.row + 2}
-          column={this.col}
-        />
-        <Cell
-          id={'' + (this.row + 2) + (this.col + 1)}
-          row={this.row + 2}
-          column={this.col + 1}
-        />
-        <Cell
-          id={'' + (this.row + 2) + (this.col + 2)}
-          row={this.row + 2}
-          column={this.col + 2}
-        />
+      <div className="cage">
+        <div className="cage__row">
+          <Cell
+            id={'' + this.row + this.col}
+            row={this.row}
+            column={this.col}
+          />
+          <Cell
+            id={'' + this.row + (this.col + 1)}
+            row={this.row}
+            column={this.col + 1}
+          />
+          <Cell
+            id={'' + this.row + (this.col + 2)}
+            row={this.row}
+            column={this.col + 2}
+          />
+        </div>
+        <div className="cage__row">
+          <Cell
+            id={'' + (this.row + 1) + this.col}
+            row={this.row + 1}
+            column={this.col}
+          />
+          <Cell
+            id={'' + (this.row + 1) + (this.col + 1)}
+            row={this.row + 1}
+            column={this.col + 1}
+          />
+          <Cell
+            id={'' + (this.row + 1) + (this.col + 2)}
+            row={this.row + 1}
+            column={this.col + 2}
+          />
+        </div>
+        <div className="cage__row">
+          <Cell
+            id={'' + (this.row + 2) + this.col}
+            row={this.row + 2}
+            column={this.col}
+          />
+          <Cell
+            id={'' + (this.row + 2) + (this.col + 1)}
+            row={this.row + 2}
+            column={this.col + 1}
+          />
+          <Cell
+            id={'' + (this.row + 2) + (this.col + 2)}
+            row={this.row + 2}
+            column={this.col + 2}
+          />
+        </div>
       </div>
     );
   }
